e8994fb726bf53ead2b498ad06b2a7af53c44bfc
Daniel Berndt <daniel.berndt@gmail.com>
2013-06-27 18:57:30 +0200

    expand action buttons behave correctly when items are collapsed in the beginning

--- jquery.nestable.js
+++ jquery.nestable.js
@@ -240,7 +240,11 @@
                 li.prepend($(this.options.expandBtnHTML));
                 li.prepend($(this.options.collapseBtnHTML));
             }
-            li.children('[data-action="expand"]').hide();
+            if (li.hasClass(this.options.collapsedClass)) {
+              li.children('[data-action="collapse"]').hide();
+            } else {
+              li.children('[data-action="expand"]').hide();
+            }
         },
 
         unsetParent: function(li)
